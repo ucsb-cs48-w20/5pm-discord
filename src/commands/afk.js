0bcb3b098caf4a2b2eb40c9bd0ff682200ce01db
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-01-29 14:10:18 -0800

    tk - working towards afk command

---
+++ src/commands/afk.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+    let reason = args.join(' ') ? args.join(' ') : 'I am currently afk, I will reply as soon possible.';
+    let afklist = bot.afk.get(message.author.id);
+
+    if (!afklist) {
+        let construct = {
+            id: message.author.id,
+            reason: reason
+        };
+        bot.afk.set(message.author.id, construct);
+        return message.reply(`you have been set to afk for reason: ${reason}`)
+            .then(msg => msg.delete(5000));
+    }
+
+};
+
+module.exports.help = {
+    name:"afk"
+};
