a9399456dfa417c76e6a1f7dcd7b2b88ad2d0238
Patrick Kuang <pkuang@ucsb.edu>
2020-02-19 01:03:09 -0800

    pk - allows user to input a serach string and will return top ten videos from youtube

--- src/commands/play.js
+++ src/commands/play.js
@@ -5,8 +5,17 @@ const youtube = new YouTube(TOKEN.googleKey);
 
 module.exports.run = async (bot, message, args) => {
     try{
+        var videos = await youtube.searchVideos(message, 10);
+        let index = 0;
+        message.channel.send(`
+__**Song selection:**__
+
+${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+
+Please provide a value to select one of the search results ranging from 1-10.
+					`);
     } catch (err) {
-        console.log("failed");
+        console.log(err);
     }
 }
 
