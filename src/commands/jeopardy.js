dab49e58a7cec6afcc075adf198900b15a1a27ae
Max LR <mleeroller@gmail.com>
2020-02-25 19:06:25 -0800

    max - created jeopardy file and branch

---
+++ src/commands/jeopardy.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+const config = require("../botconfig");
+//Usage: ?filter arg1 arg2 arg3...
+//case-agnostic args, ?filter with no args to reset
+//no memory of keywords so resets every time its called, could change?
+module.exports.run = async (bot, message, args) => {
+//you expected an actual function, but it was me, STUB
+};
+
+module.exports.help = {
+    name:"jeopardy"
+};
