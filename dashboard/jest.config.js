c5a408fce181d61ed9bd4064dffff75af6b33e93
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-02-27 18:28:21 -0800

    tk - added functional and unit tests for button component

--- dashboard/jest.config.js
+++ dashboard/jest.config.js
@@ -1,31 +1,24 @@
 module.exports = {
-    // The root of your source code, typically /src
-    // `<rootDir>` is a token Jest substitutes
-    roots: ["<rootDir>/src"],
-  
-    // Jest transformations -- this adds support for TypeScript
-    // using ts-jest
-    transform: {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-  
-    // Runs special logic, such as cleaning up components
-    // when using React Testing Library and adds special
-    // extended assertions to Jest
-    setupFilesAfterEnv: [
-      "@testing-library/react/cleanup-after-each",
-      "@testing-library/jest-dom/extend-expect"
+    "roots": [
+        "<rootDir>/src"
     ],
-  
-    // Test spec file resolution pattern
-    // Matches parent folder `__tests__` and filename
-    // should contain `test` or `spec`.
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  
-    // Module file extensions for importing
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "snapshotSerializers": ["enzyme-to-json/serializer"],
+    "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    },
+    "setupFilesAfterEnv": ["<rootDir>/src/setupTests.ts"]
 
-    snapshotSerializers: ["enzyme-to-json/serializer"],
-    setupTestFrameworkScriptFile: "<rootDir>/src/setupEnzyme.ts"
-  };
-  
+}
---
+++ dashboard/src/components/Content/content.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Content from './content';
+
+describe('Content', () => {
+    it('should be defined', () => {
+        expect(Content).toBeDefined();
+    });
+});
