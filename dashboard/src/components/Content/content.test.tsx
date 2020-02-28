32f529cc12fcc35c2918f42728878032319e752e
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-02-27 19:10:16 -0800

    tk - fixed jest version

--- dashboard/src/components/Content/content.test.tsx
+++ dashboard/src/components/Content/content.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Content from './content';
+import handleInvite from './content';
 
 describe('Content', () => {
     it('should be defined', () => {
