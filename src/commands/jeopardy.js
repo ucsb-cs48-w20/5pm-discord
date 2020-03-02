ac83d8593bc10c8e04021420abd4c6fbb5aade38
Max LR <mleeroller@gmail.com>
2020-03-02 02:27:14 -0800

    max - jeopardy recognizes correct answers, pretty buggy still

--- src/commands/jeopardy.js
+++ src/commands/jeopardy.js
@@ -3,106 +3,72 @@ const errors = require("../utils/errors.js");
 const config = require("../botconfig");
 const {get} = require('snekfetch');
 var liveRound = 0;
-//Usage: ?jeopardy to start jeopardy session. After 10s queue, will start with a random question and then take most recent correct's answer
-//Stage 1: Asks questions and (CURRENTLY HERE) recognizes if a message contains correct answer
-//Stage 3: Increments/decrements score correctly
 
 module.exports.run = async (bot, message, args) => {
-    // if(args == "" && liveRound == 0){
-    //     message.channel.send(`Welcome to Jeopardy! Start/stop questions with ?jeopardy resume/pause. View scores with ?jeopardy scores. View rules with ?jeopardy rules. End the round with ?jeopardy end.`);
-    //     liveRound = 1;
-    // }
-    // if(args == "resume" && liveRound == 1){
-    //     //begin questions. RULES: First correct answer only. After 10 seconds of no correct answer, announce answer and post new category. 3s later, question..
-    //     let { answer, question, value, category } = await getQuestion();
-    //     answer = answer.replace(/<(?:.|\n)*?>/gm, '');
-    //     answer = answer.toLowerCase();
+    if(args == ""){ //Good for now?
+        if(liveRound == 0){
+            message.channel.send(`Welcome to Jeopardy, hosted by ${message.author}! Start/stop questions with ?jeopardy resume/pause. View scores with ?jeopardy scores. View rules with ?jeopardy rules. The host can end the round with ?jeopardy end.`);
+            var host = message.author;
+            var players = [];
+            var scores = [];
+            var playerCount = 0;
+            liveRound = 1;
+        }
+        else if(liveRound == 1){
+            message.channel.send(`There is already a live round of Jeopardy.`);
+        }
+    }
+    if(args == "resume"){ //TODO: Timer
+        if(liveRound == 0){
+            message.channel.send(`There isn't a live round of Jeopardy. Start one with ?jeopardy.`);
+        }
+        if(liveRound == 1){
+            let { answer, question, value, category } = await getQuestion();
+            answer = answer.replace(/<(?:.|\n)*?>/gm, '').toLowerCase();
 
-    //     console.log(answer); //for bugtesting, remove before merging to master
+            console.log(answer); //for bugtesting, remove before merging to master
 
-    //     if (value == null) {
-    //         value = 200;
-    //     }
-
-    //     message.channel.send(`The category is ${category.title} for ${value}.`);
-    //     message.channel.send(question);
-    //     bot.on('message',  async message => {
-    //         if(message.content.toLowerCase().includes(answer)&&!message.author.bot){
-    //             message.channel.send(`${message.author} is correct and receives $${value}.`);
-    //         }
-    //     });
-    // }
-    // if(args == "pause" && liveRound == 1){
-    //     //pause timer
-    // }
-    // if(args == "end" && liveRound == 1){
-    //     liveRound = 0;
-    // }
-    // if(args == "scores"){
-    //     //print all scores for current round
-    // }
-
-//this might be cleaner with a switch
-    switch(args){ //implement the question timer
-        case 0:
-            args = "";
-            if(liveRound = 1){
-                message.channel.send(`There's already a live round of Jeopardy.`);
-            } else {
-                message.channel.send(`Welcome to Jeopardy! Start/stop questions with ?jeopardy resume/pause. View scores with ?jeopardy scores. View rules with ?jeopardy rules. End the round with ?jeopardy end.`);
-                liveRound = 1; 
+            if (value == null) {
+                value = 200;
             }
-            break;
-        case 1:
-            args = "resume";
-            if(liveRound = 0){
-                message.channel.send(`There's no live round of Jeopardy.`);
-            }
-            if(liveRound = 1){
-                let { answer, question, value, category } = await getQuestion();
-                answer = answer.replace(/<(?:.|\n)*?>/gm, '');
-                answer = answer.toLowerCase();
 
-                console.log(answer); //for bugtesting, remove before merging to master
-
-                if (value == null) {
-                    value = 200;
+            message.channel.send(`The category is ${category.title} for ${value}.`);
+            message.channel.send(question);
+            bot.on('message',  async message => { //TODO: Score parsing improvements (optional answers), Score Tracking
+                if(message.content.toLowerCase().includes(answer)&&!message.author.bot){
+                    message.channel.send(`${message.author} is correct and receives $${value}.`);
                 }
-
-                message.channel.send(`The category is ${category.title} for ${value}.`);
-                message.channel.send(question);
-                bot.on('message',  async message => {
-                    if(message.content.toLowerCase().includes(answer)&&!message.author.bot){ //need to parse optional answers and filter for question format
-                        message.channel.send(`${message.author} is correct and receives $${value}.`);
-                        // for(var i = 0; i < players.length; i++){ finish this for score tracking
-                        //     if(players[i] = message.author)
-                        // }
-                        // players[playerCount] = message.author;
-                    }
-                });
-            }
-            break;
-        case 2:
-            args = "pause";
-            //pause the question timer
-            break;
-        case 3:
-            args = "end";
+            });
+        }
+    }
+    if(args == "pause"){ //finish this
+        //pause timer
+    }
+    if(args == "reset"){//FOR TESTING, REMOVE BEFORE MASTER MERGE
+        liveRound = 0;
+    }
+    if(args == "end"){ //finish this, bugged pretty hard rn
+        //if(message.author == host){
             if(liveRound == 1){
                 liveRound = 0;
-                message.channel.send(`Jeopardy is over. The winner is PLACEHOLDER with MONEY!`);
-            } else {
-                message.channel.send(`There's no live round of Jeopardy.`)
+                var maxScore = 0;
+                for(var i = 0; i < scores.length; i++){
+                    if(scores[i] > maxScore){
+                        maxScore = i;
+                    }
+                }
+                message.channel.send(`Jeopardy is over. The winner is ${players[maxScore]} with $${scores[maxScore]}!`)
             }
-            break;
-        case 4:
-            args = "scores";
-            //return scores
-            break;
-        case 5:
-            args = "rules";
-            //explain rules
-            break;
+        //} 
+        else{
+            message.channel.send(`There isn't a live round of Jeopardy. Start one with ?jeopardy.`);
+        }
+    }
+    if(args == "scores"){
+        //print all scores for current round
+    }
+    if(args == "rules"){
+        //print rules
     }
 };
 
