14f3c19bc91ed19866772ce5a352635cda0ec313
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-03-12 14:36:19 -0700

    tk - fixed ban message

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -11,16 +11,16 @@ module.exports.run = async (bot, message, args) => {
     const userToBan = message.mentions.users.first();
     let banReason = args.slice(1).join(' ');
     banReason = banReason.substring(1, banReason.length - 1);
-    
+
     if (userToBan) {
         if (message.author === userToBan) {
             return message.channel.send(`${message.author} You cannot ban yourself.`);
         }
         else {
-            userToBan.send(banReason);
             if (!banReason) {
                 banReason = "unacceptable behavior"
             }
+            await userToBan.send("You have been banned for: " + banReason);
             await message.guild.ban(userToBan) // Bans the user
             const banConfirmationEmbed = new Discord.RichEmbed()
             .setColor('RED')
@@ -32,8 +32,8 @@ module.exports.run = async (bot, message, args) => {
                 msg.delete(10000)
             });
         }
-    } else { 
-        return message.channel.send(`${message.author} Invalid format. Please ban in the following format: ?ban @thisUser#2131 Spam`);
+    } else {
+        return message.channel.send(`${message.author} Invalid format or User is not in the server. Please ban in the following format: ?ban @thisUser#2131 Spam or Try another User.`);
     }
 };
 
